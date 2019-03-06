581173e5f48048672780948ef0c1249a8c9e8e6b
imartinezl <inigomlap@gmail.com>
2019-03-06 23:36:55 +0100

    Code cleaning + Data importation

--- data_exploration.R
+++ data_exploration.R
@@ -1,23 +1,34 @@
-# Data Exploration
+
+# Libraries ---------------------------------------------------------------
 
 library(dplyr)
 library(zeallot)
 
-data <- read.csv('data/Contenedores_vidrio_con_publicidad.csv', sep = ";", stringsAsFactors = F) %>% 
+# Data Importation --------------------------------------------------------
+
+data_vidrio <- read.csv('data/Contenedores_vidrio_con_publicidad.csv', sep = ";", dec = ".", stringsAsFactors = F) %>% 
   dplyr::mutate(FECHA.PUESTA.EN.SERVICIO = as.Date(FECHA.PUESTA.EN.SERVICIO, format="%d/%m/%Y"),
                 Longitud = as.numeric(sub(",", ".", Longitud, fixed = TRUE)),
                 Latitud = as.numeric(sub(",", ".", Latitud, fixed = TRUE)))
 
-a <- rgdal::readOGR("data/Madrid_Central/Madrid_Central.shp",
-                    layer = "Madrid_Central", GDAL1_integer64_policy = TRUE)
-mylines <- sp::spTransform(a, sp::CRS("+init=epsg:4326"))
+data_puntos_limpios <- read.csv('data/200284-0-puntos-limpios.csv', sep = ";", stringsAsFactors = F)
+data_contenedores_varios <- read.csv('data/Contenedores_varios.csv', sep = ";", dec = ",",stringsAsFactors = F)
+data_puntos_limpios <- read.csv('data/ContenedoresRopa.csv', sep = ";", dec = ",", stringsAsFactors = F) %>% 
+  dplyr::mutate(LONGITUD = as.numeric(sub("\\s", "", LONGITUD)),
+                LATITUD = as.numeric(sub("\\s", "", LATITUD)) )
+data_puntos_limpios <- read.csv('data/200284-0-puntos-limpios.csv', sep = ";", stringsAsFactors = F)
 
-data %>% 
+madrid_central <- rgdal::readOGR("data/Madrid_Central/Madrid_Central.shp",
+                    layer = "Madrid_Central", GDAL1_integer64_policy = TRUE)
+madrid_central <- sp::spTransform(madrid_central, sp::CRS("+init=epsg:4326"))
+
+data_vidrio %>% 
   ggplot2::ggplot()+
   ggplot2::geom_point(ggplot2::aes(x=Longitud, y=Latitud))
 
 
-library(leaflet)
+# Leaflet -----------------------------------------------------------------
+
 icons <- leaflet::awesomeIcons(
   icon = 'ios-close',
   iconColor = 'black',
@@ -25,28 +36,22 @@ icons <- leaflet::awesomeIcons(
   markerColor = "orange"
 )
 
-epsg32630 <- leaflet::leafletCRS(
-  crsClass = "L.Proj.CRS",
-  code = "EPSG:32630",
-  proj4def = a@proj4string@projargs,
-  bounds = a@bbox,
-  resolutions = 2^(16:7))
-
-
-leaflet::leaflet(data = data, options = leaflet::leafletOptions(minZoom = 0, maxZoom = 18)) %>% 
+leaflet::leaflet(options = leaflet::leafletOptions(minZoom = 0, maxZoom = 18)) %>% 
   # leaflet::addTiles() %>%
   leaflet::addProviderTiles(leaflet::providers$Wikimedia, group = "Tiles") %>%
-  leaflet::addCircles(lat = ~ Latitud, lng = ~ Longitud, group = "Circles") %>%
-  leaflet::addAwesomeMarkers( lat = ~ Latitud, lng = ~ Longitud, label = ~Nombre,
+  leaflet::addCircles(data = data_vidrio, lat = ~ Latitud, lng = ~ Longitud, label = ~Nombre, group = "Circles") %>%
+  leaflet::addAwesomeMarkers(data = data_vidrio, lat = ~ Latitud, lng = ~ Longitud, label = ~Nombre,
                              icon = icons, labelOptions = leaflet::labelOptions(textsize = "15px"),
                              group = "AwesomeMarkers") %>%
-  leaflet::addPolygons(data = mylines, group = "Madrid Central") %>% 
+  leaflet::addPolygons(data = madrid_central, group = "Madrid Central") %>% 
   leaflet::addLayersControl(
     overlayGroups = c("Tiles", "Circles", "AwesomeMarkers","Madrid Central"),
     options = leaflet::layersControlOptions(collapsed = FALSE)
   )
 
 
+
+# Mapdeck -----------------------------------------------------------------
 
 library(mapdeck)
 mapdeck::set_token(Sys.getenv("MAPBOX"))
