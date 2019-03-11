c1d0b7579388c2cdc3181898957d4d28933163bf
imartinezl <inigomlap@gmail.com>
2019-03-11 20:50:53 +0100

    Route optimization first approach

---
+++ route_optimization.R
@@ -0,0 +1,73 @@
+# Route Optimization
+
+# Libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(zeallot)
+
+# Data Importation --------------------------------------------------------
+
+# Madrid Central
+madrid_central <- rgdal::readOGR("data/Madrid_Central/Madrid_Central.shp", GDAL1_integer64_policy = TRUE)
+madrid_central <- sp::spTransform(madrid_central, sp::CRS("+init=epsg:4326"))
+
+# Contenedores Vidrio
+data_contenedores_vidrio <- read.csv('data/Contenedores_vidrio_con_publicidad.csv', sep = ";", dec = ".", stringsAsFactors = F) %>% 
+  dplyr::mutate(FECHA.PUESTA.EN.SERVICIO = as.Date(FECHA.PUESTA.EN.SERVICIO, format="%d/%m/%Y"),
+                LONGITUD = as.numeric(sub(",", ".", Longitud, fixed = TRUE)),
+                LATITUD = as.numeric(sub(",", ".", Latitud, fixed = TRUE)),
+                Nombre = stringr::str_to_title(Nombre),
+                LABEL = paste0("<b>Dirección:</b> ", Nombre))
+
+points <- data_contenedores_vidrio %>% 
+  dplyr::slice(1:10) %>% 
+  dplyr::select(LONGITUD, LATITUD) %>% 
+  plyr::rename(c("LONGITUD"="longitude", "LATITUD"="latitude"))
+
+i <- 1
+j <- 2
+waypoint0 <- paste0(points$latitude[i],',',points$longitude[i])
+waypoint1 <- paste0(points$latitude[j],',',points$longitude[j])
+mode <- "fastest;truck;traffic:disabled"
+
+get.route <- function(waypoint0, waypoint1, mode){
+  base <- "https://route.api.here.com/routing/7.2/"
+  endpoint <- "calculateroute.json"
+  app_id <- Sys.getenv("HERE_APP_ID")
+  app_code <- Sys.getenv("HERE_APP_CODE")
+  
+  url <- paste0(base,endpoint,"?","app_id=", app_id, "&app_code=", app_code, 
+                "&waypoint0=geo!", waypoint0, "&waypoint1=geo!", waypoint1, 
+                "&mode=", mode)
+  tryCatch({
+    r <- httr::GET(url)
+    httr::stop_for_status(response)
+    content <- jsonlite::fromJSON(httr::content(r, as="text"))
+    return(content)
+  }, http_error=function(e) {
+    ## log error or otherwise recover
+    warning(paste0("HTTP error ", e))
+  })
+}
+extract.route.info <- function(content){
+  distance <- content$response$route$summary$distance
+  traffic_time <- content$response$route$summary$trafficTime
+  base_time <- content$response$route$summary$baseTime
+  route_lat <- content$response$route$leg[[1]]$maneuver[[1]]$position$latitude
+  route_long <- content$response$route$leg[[1]]$maneuver[[1]]$position$longitude
+  route_dist <- content$response$route$leg[[1]]$maneuver[[1]]$length
+  route_time <- content$response$route$leg[[1]]$maneuver[[1]]$travelTime
+  start_street_side <- content$response$route$leg[[1]]$start$sideOfStreet
+  start_street_name <- content$response$route$leg[[1]]$start$mappedRoadName
+  stop_street_side <- content$response$route$leg[[1]]$end$sideOfStreet
+  stop_street_name <- content$response$route$leg[[1]]$end$mappedRoadName
+  
+  data.frame(distance, traffic_time, base_time, route_lat, route_long,
+             route_dist, route_time, start_street_side, start_street_name, 
+             stop_street_side, stop_street_name)
+}
+
+d <- get.route(waypoint0, waypoint1, mode) %>% extract.route.info()
+data.frame(i,j) %>% 
+  cbind(d)
+
