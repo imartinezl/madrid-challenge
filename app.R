653cf9acb0c9025e2b9944928ab20a79eea7d6ce
imartinezl <inigomlap@gmail.com>
2019-05-29 20:08:56 -0400

    Simulated annealing parameters numeric inputs

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ library(zeallot)
 
 # Data Visualization ------------------------------------------------------
 
-source('data_exploration.R')
+#source('data_exploration.R')
 
 # Route Optimization ------------------------------------------------------
 
@@ -17,14 +17,28 @@ source('route_opt.R')
 
 ui <- shiny::fluidPage(
   theme = shinythemes::shinytheme("united"),
-  leaflet::leafletOutput("mymap")
+  shiny::titlePanel("Tabsets"),
+  shiny::mainPanel(width = 12,
+    shiny::tabsetPanel(
+      shiny::tabPanel("Explorar Mapa", leaflet::leafletOutput("mymap", width = "100%", height="800px")),
+      shiny::tabPanel("Optimizar Ruta", 
+                      shiny::h3("Simulated Annealing Parameters"),
+                      shiny::numericInput("num", "S-curve Amplitude", value = 4000),
+                      shiny::numericInput("num", "S-curve Center", value = 0),
+                      shiny::numericInput("num", "S-curve Width", value = 3000),
+                      shiny::numericInput("num", "Total Iterations", value = 25000),
+                      shiny::numericInput("num", "Draw Map Every N Iterations", value = 500)
+                     )
+    )
+  )
+  #leaflet::leafletOutput("mymap", width = "100%", height="800px")
   
 )
 
 server <- function(input, output, session) {
   
   output$mymap <- leaflet::renderLeaflet({
-    data_exploring_map
+    #data_exploring_map
   })
   
 }
